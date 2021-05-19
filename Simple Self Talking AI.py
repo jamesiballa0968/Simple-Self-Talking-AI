2e2350bf367acf16909d472bb12c0c57d41b7b1d
jamesiballa0968 <84361773+jamesiballa0968@users.noreply.github.com>
2021-05-19 22:33:39 +0800

    v.1 Simple_Self_TalkingAI
    First python program.

---
+++ Simple Self Talking AI.py
@@ -0,0 +1,29 @@
+print("Simple Self-Talking AI")
+print("------------------------")
+print("Hello and Good morning!")
+print("This is James AI")
+print("Thank you for taking this simple Self Talking Python program")
+print("I would like to ask some of your information.")
+print("")
+name = input("What is your name?: ")
+
+print("Hmmm... so your " + name)
+print("Hello " + name + " nice to meet you.")
+print("")
+age = input("May I know your age " + name + "?: ")
+print("")
+
+address = input("May I know your address too?: ")
+print("")
+
+hobby = input("..aaaand your hobby too? ")
+print("")
+
+print("So you are " + name + ", " + age + " years old. You lived at " + address + ". And your hobby is "+hobby+".")
+print("")
+print("I think that's it " + name + ".")
+print("------------------------------------")
+print("Well I need to leave you now " + name + " and nice to meet you.")
+print("This is James AI commencing for log out......")
+
+# First Program on Python
